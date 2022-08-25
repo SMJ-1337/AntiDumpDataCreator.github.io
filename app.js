67a7bc5465f58bb423eeb007bd2c89c545f76282
SMJ-1337 <72356098+SMJ-1337@users.noreply.github.com>
2022-08-25 14:35:27 +0200

    First Upload
    Thats the first Upload for the creator
    
    v1.0

---
+++ app.js
@@ -0,0 +1,25 @@
+const FrontendData = `data`;
+const JsonData = `data`;
+
+function Frontend() {
+    alert("Frontend: created!");
+    download("frontend.json", JSON.stringify({ Data: FrontendData }));
+}
+
+function JavaScript() {
+    alert("JavaScriptData created!");
+    download("javascript.json", JSON.stringify({ Data: JsonData }));
+}
+
+function download(filename, text) {
+    var element = document.createElement('a');
+    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+    element.setAttribute('download', filename);
+  
+    element.style.display = 'none';
+    document.body.appendChild(element);
+  
+    element.click();
+  
+    document.body.removeChild(element);
+}
